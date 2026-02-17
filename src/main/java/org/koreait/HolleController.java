030c5741caa7ed71065fb5222b203cbefc4b5437
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-02-17 16:17:01 +0900

    실행 완료

---
+++ src/main/java/org/koreait/HolleController.java
@@ -0,0 +1,10 @@
+package org.koreait;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HolleController {
+    public static void main(String[] args) {
+        System.out.println(" 수정아 사랑해 나랑 결혼 하자 ");
+    }
+}
