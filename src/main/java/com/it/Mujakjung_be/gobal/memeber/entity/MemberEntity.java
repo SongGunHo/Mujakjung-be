4466cb503159ad1927ce00054d8ba00b419de957
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-02 20:49:17 +0900

    init

--- src/main/java/com/it/Mujakjung_be/gobal/memeber/entity/MemberEntity.java
+++ src/main/java/com/it/Mujakjung_be/gobal/memeber/entity/MemberEntity.java
@@ -1,6 +1,39 @@
-package com.it.Mujakjung_be.gobal.memeber.entity;
-import jakarta.persistence.Entity;
 
-@Entity
+package com.it.Mujakjung_be.gobal.memeber.entity;
+
+import jakarta.persistence.*;
+
+/**
+ * 회원 정보를 저장하는 JPA 엔티티 클래스
+ * → DB의 member 테이블과 매핑됨
+ */
+@Entity //  이 클래스가 JPA 엔티티임을 선언 (테이블과 매핑)
+@Table(name = "member") // DB 테이블 이름 지정
 public class MemberEntity {
+
+    @Id // 기본키(PK) 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // MySQL auto_increment 사용
+    private Long Id;
+
+    @Column(nullable = false, length = 50)
+    //  NOT NULL + VARCHAR(50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    //  NOT NULL + VARCHAR(100)
+    private String password;
+
+    @Column(nullable = false , length = 30)
+    //  NOT NULL + VARCHAR(30)
+    private String name;
+
+    /**
+     * JPA 기본 생성자 (필수)
+     * - JPA가 리플렉션으로 객체 생성할 때 사용
+     * - protected 또는 public 이어야 함
+     */
+    protected MemberEntity(){
+
+    }
 }
